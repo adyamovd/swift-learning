a51362118195d0126f376b7879a022ae620a95bc
Dias <adyamov_dias@mail.ru>
2025-01-08 23:11:03 +0500

    Add files via upload

--- 3. Введение в Swift/3.4_Adyamov/1/3.4.1_Adyamov.playground/Contents.swift
+++ 3. Введение в Swift/3.4_Adyamov/1/3.4.1_Adyamov.playground/Contents.swift
@@ -5,9 +5,9 @@ var surnameString = "Surname: Adyamov\n"
 var ageString = "Age: 26\n"
 var hobbyString = "Hobbies: Cars, Hip-Hop, Football\n"
 var salaryString = "Salary expectations: 4444 USD"
-var sumString = nameString+surnameString+ageString
+var sumString = nameString + surnameString + ageString
 sumString += hobbyString
-sumString.append (salaryString)
+sumString.append(salaryString)
 print(sumString)
 let nameAgainString = "Dias"
 nameAgainString.forEach { char in

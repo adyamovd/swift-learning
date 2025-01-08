f32d4049ef36e738c26d420f466965ada5afafda
Dias <adyamov_dias@mail.ru>
2025-01-08 16:00:12 +0500

    Add files via upload

---
+++ 3. Введение в Swift/3.4_Adyamov/1/3.4.1_Adyamov.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var nameString = "Name: Dias \u{1F408}\n"
+var surnameString = "Surname: Adyamov\n"
+var ageString = "Age: 26\n"
+var hobbyString = "Hobbies: Cars, Hip-Hop, Football\n"
+var salaryString = "Salary expectations: 4444 USD"
+var sumString = nameString+surnameString+ageString
+sumString+=hobbyString
+sumString.append(salaryString)
+print(sumString)
+let nameAgainString = "Dias"
+nameAgainString.forEach { char in
+    print(char)
+}
+

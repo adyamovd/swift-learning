c23f9eb56cc9a608685456465bed247c48282c85
Dias <adyamov_dias@mail.ru>
2025-01-08 15:56:27 +0500

    Add files via upload

--- 3.4_Adyamov/1/3.4.1_Adyamov.playground/Contents.swift
+++ 3.4_Adyamov/1/3.4.1_Adyamov.playground/Contents.swift
@@ -6,11 +6,11 @@ var ageString = "Age: 26\n"
 var hobbyString = "Hobbies: Cars, Hip-Hop, Football\n"
 var salaryString = "Salary expectations: 4444 USD"
 var sumString = nameString+surnameString+ageString
-sumString += hobbyString
+sumString+=hobbyString
 sumString.append(salaryString)
 print(sumString)
 let nameAgainString = "Dias"
 nameAgainString.forEach { char in
-    print (char)
+    print(char)
 }
 
--- 3.4_Adyamov/2/3.4.2_Adyamov/3.4_Adyamov/main.swift
+++ 3.4_Adyamov/2/3.4.2_Adyamov/3.4_Adyamov/main.swift
@@ -5,14 +5,15 @@
 //  Created by Диас Адямов  on 07.01.2025.
 //
 import Foundation
-print("Введите исходный текст")
-var inputText = readLine()
-print("Введите текст для замены")
-var inputChangeText = readLine()
-print ("На что нужно заменить \"ли\"?")
-var inputReplaceText = readLine()
-print ("Результат:")
-var string = "А не спеть ли мне песню о любви? А не выдумать ли новый жанр?"
-string.replace("ли", with:"ка")
-print(string)
-
+print("Исходный текст")
+var inputString = "А не спеть ли мне песню о любви? А не выдумать ли новый жанр?"
+print(inputString)
+print("Текст для замены")
+let changeString = "ли"
+print(changeString)
+print("Меняем \"\(changeString)\" на:")
+let replaceString = "ка"
+print(replaceString)
+print("Результат выполения программы:")
+inputString.replace(changeString, with:replaceString)
+print(inputString)

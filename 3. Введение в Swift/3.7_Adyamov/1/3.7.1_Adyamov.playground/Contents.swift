29363f2e67055863e18c8c909ef112e671ff0f77
Dias <adyamov_dias@mail.ru>
2025-01-16 23:29:27 +0500

    Add files via upload

---
+++ 3. Введение в Swift/3.7_Adyamov/1/3.7.1_Adyamov.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+let movies = ["Властелин колец", "Тренировочный день", "Остров проклятых"]
+for item in movies {
+    print(item)
+}
---
+++ 3. Введение в Swift/3.7_Adyamov/3/3.7.3_Adyamov.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var arrayBoys = ["Диас", "Влад", "Костя", "Милан", "Окно", "Одеяло", "Газ", "Запах", "Мальчик", "Пацан", "Бро", "Черт"]
+arrayBoys.insert("Маша", at:2)
+arrayBoys.insert(contentsOf: ["Люся", "Вафлюша", "Нюша", "Хрюша", "Оксана"], at:3)
+arrayBoys.insert("Самал", at:5)
+arrayBoys.removeFirst(4)
+arrayBoys.removeLast(5)
+arrayBoys.sorted()
+

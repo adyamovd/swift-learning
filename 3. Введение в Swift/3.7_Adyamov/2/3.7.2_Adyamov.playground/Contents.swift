4192f0863784f540a67656dff5881a8bed363e2a
Dias <adyamov_dias@mail.ru>
2025-01-17 16:01:35 +0500

    Add files via upload

--- 3. Введение в Swift/3.7_Adyamov/2/3.7.2_Adyamov.playground/Contents.swift
+++ 3. Введение в Swift/3.7_Adyamov/2/3.7.2_Adyamov.playground/Contents.swift
@@ -2,14 +2,13 @@ import UIKit
 
 var arrayIntegers = [5,7,10,22,30]
 print(arrayIntegers.count) // вывел количество элементов
-arrayIntegers[0]+=arrayIntegers.count
+arrayIntegers[0] += arrayIntegers.count
 print(arrayIntegers) // проверил что сработало
 var resultArray: [Int] = []
-var value = 1
-var resultValue: Int
-for i in 0..<arrayIntegers.count {
-    arrayIntegers[i] = arrayIntegers[i] * value
+var resultValue = 1
+for value in arrayIntegers {
+    resultValue *= value
 }
-arrayIntegers.append(value)
+arrayIntegers.append(resultValue)
 print(arrayIntegers)
-// брат остальные задания сделал и думаю нормально сделал. в аппенд пока вставил просто вэлью, попробовал объявить короче как мы обсудили переменную перед циклом, и я ща даже примерно догадываюсь почему считает так, как считает, но как пофиксить не понимаю, в гугле тоже херня блин, там в основном как один массив перемножить с другим (как и в уроке было в задаче в принципе. буду признателен за подсказку ))
+

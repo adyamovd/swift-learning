a7e9fd00571bf958aaa12a44a481f44edfbe45f8
Dias <adyamov_dias@mail.ru>
2025-01-23 22:55:02 +0500

    Add files via upload

--- 3. Введение в Swift/3.9_Adyamov/2/3.9.2_Adyamov.playground/Contents.swift
+++ 3. Введение в Swift/3.9_Adyamov/2/3.9.2_Adyamov.playground/Contents.swift
@@ -1,12 +1,25 @@
 import UIKit
 
-var initialDictionary = ["Диас Адямов": 4444, "Влад Форафонтов": 1998, "Костя Китаев": 1999, "Юра из сбера": 10000, "Тоха из культа": 5000]
-var secondDictionary = ["Жубик Жубиков": 3489, "Влад Форафонтов": 2000, "Дженни Владова": 4000, "Тоха из культа": 1000]
-var summ = 0
-for (key,value) in initialDictionary {
-    let secondValue = secondDictionary[key] ?? 0
-    let summValue = initialDictionary[key] ?? 0
-    summ = secondValue + summValue
-    print("\(key): \(summ)")// в результате суммируются значения первого словаря + значения дубликатов из второго словаря, но пока не добавляются несовпадающие ключи из второго словаря, хотя цикл явно по ним проходит, раз уж корректирует значения дубликатов. сначала цикл прописал по ключу for key in initial dictionary - все работало точно также. потом появилась мысль через валью секондДикшионари подтянуть его ключи )) не сработало. писал что-то вроде let secondKey = secondValue[value] ?? "ошибка". нагуглил функцию merge. но там какой-то странный синтаксис, попробовал, не запустилось нифига, как будто шляпа какая-то, а не решение.
-}
+    var initialDictionary = ["Диас Адямов": 4444, "Влад Форафонтов": 1998, "Костя Китаев": 1999, "Юра из сбера": 10000, "Тоха из культа": 5000]
+    var secondDictionary = ["Жубик Жубиков": 3489, "Влад Форафонтов": 2000, "Дженни Владова": 4000, "Тоха из культа": 1000]
+    var keysArray = Array(initialDictionary.keys)
+    var secondKeysArray = Array(secondDictionary.keys)
+    var resultArray = keysArray + secondKeysArray
+    print(resultArray) // проверка что работает ок
+    var resultDictionary: [String: Int] = [:]
+    for key in resultArray {
+        guard resultDictionary[key] == nil
+        else {
+            continue }
+        let value1 = initialDictionary[key]
+        let value2 = secondDictionary[key]
+        if let value1, let value2 {
+            resultDictionary[key] = value1 + value2
+        } else if let value1 {
+            resultDictionary[key] = value1
+        } else if let value2 {
+            resultDictionary[key] = value2
+        }
+    }
+    print(resultDictionary)
 

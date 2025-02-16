f41b255f05e5cf339306bacf3ee165f36689320e
Dias <adyamov_dias@mail.ru>
2025-02-16 17:10:07 +0500

    Add files via upload

--- 3. Введение в Swift/3.13_Adyamov/1/3.13.playground/Contents.swift
+++ 3. Введение в Swift/3.13_Adyamov/1/3.13.playground/Contents.swift
@@ -7,18 +7,20 @@ enum Grade: String {
 }
 
 enum Developer {
-    case iOS(grade:Grade)
-    case Android(grade:Grade)
+    case iOS(grade: Grade)
+    case android(grade: Grade)
+    
     func getDescription() -> String {
         switch self {
         case .iOS(let grade):
             return "\u{1F34F}\(grade.rawValue) iOS-developer"
-        case .Android(let grade):
+        case .android(let grade):
             return "\u{1F916}\(grade.rawValue) Android-developer"
         }
     }
 }
-let developers: [Developer] = [.iOS(grade: .junior), .Android(grade: .senior)]
+
+let developers: [Developer] = [.iOS(grade: .junior), .android(grade: .senior)]
 for developer in developers {
     print(developer.getDescription())
 }
